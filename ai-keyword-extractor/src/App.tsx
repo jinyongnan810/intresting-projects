de1d00001270ca6ed997ebebf65fe1bba4cf1eb7
Kin Yuunan <yuunan.kin@gmail.com>
2023-03-26 10:51:49 +0900

    basic styles

--- ai-keyword-extractor/src/App.tsx
+++ ai-keyword-extractor/src/App.tsx
@@ -1,5 +1,12 @@
+import { Container, Box } from "@chakra-ui/react";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={100}>
+      <Container maxWidth="3xl" centerContent>
+        <h1>App</h1>
+      </Container>
+    </Box>
+  );
 };
 
 export default App;
