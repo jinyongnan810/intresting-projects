181559664d98dbd938e4b8a2594197787b8b7ae5
Kin Yuunan <yuunan.kin@gmail.com>
2023-04-09 08:18:06 +0900

    import fonts

--- next-13/app/layout.tsx
+++ next-13/app/layout.tsx
@@ -1,4 +1,9 @@
+import { Poppins } from "next/font/google";
 import "./globals.css";
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Next.js 13 demo",
@@ -13,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>
+        <main className="container">{children}</main>
+      </body>
     </html>
   );
 }
