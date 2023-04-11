d60365ec526374a333a9d699ff3fae5d81483d06
Kin Yuunan <yuunan.kin@gmail.com>
2023-04-12 07:54:37 +0900

    faicon & repos page

--- next-13/app/code/repos/page.tsx
+++ next-13/app/code/repos/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { FaStar, FaEye, FaCodeBranch } from "react-icons/fa";
+
 const getRepos = async () => {
   const response = await fetch(
     "https://api.github.com/users/jinyongnan810/repos"
@@ -8,9 +11,40 @@ const getRepos = async () => {
 };
 
 const ReposPage = async () => {
-  const repos = await getRepos();
-  console.log(JSON.stringify(repos));
-  return <div>ReposPage</div>;
+  const repos: {
+    id: string;
+    name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    watchers_count: number;
+  }[] = await getRepos();
+  return (
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ReposPage;
