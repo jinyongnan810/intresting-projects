2a13f550b9a2291f8fee1a8c62c3f28074ecaefe
Kin Yuunan <yuunan.kin@gmail.com>
2023-04-18 08:16:38 +0900

    get query & body in api

--- next-13/app/api/courses/route.ts
+++ next-13/app/api/courses/route.ts
@@ -1,7 +1,20 @@
 import { NextResponse } from "next/server";
 import courses from "./data.json";
 
-// access with localhost:3000/api/courses
+// access with GET localhost:3000/api/courses
 export async function GET(request: Request) {
+  // to get search params
+  // localhost:3000/api/courses?query=react
+  const { searchParams } = new URL(request.url);
+  console.log(searchParams.get("query"));
   return NextResponse.json(courses);
+}
+
+// access with POST localhost:3000/api/courses
+export async function POST(request: Request) {
+  // to get request body
+  const body: { title: string; description: string } = await request.json();
+  const { title, description } = body;
+  console.log(`title: ${title}, description: ${description}.`);
+  return NextResponse.json({ message: "ok" });
 }
