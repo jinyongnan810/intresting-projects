d3dfab26d8de66f437fff967a7cb423546f64877
Kin Yuunan <yuunan.kin@gmail.com>
2023-05-03 07:53:08 +0900

    image shrinker day1

---
+++ image_shrinker/lib/components/file_dropper.dart
@@ -0,0 +1,33 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FileDropper extends ConsumerWidget {
+  const FileDropper({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: DottedBorder(
+        dashPattern: const [20, 8],
+        color: Theme.of(context).primaryColor,
+        strokeWidth: 5,
+        strokeCap: StrokeCap.round,
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(20),
+        child: SizedBox(
+          width: double.infinity,
+          height: 200,
+          child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                '+',
+                style: TextStyle(
+                    fontSize: 50, color: Theme.of(context).primaryColor),
+              )),
+        ),
+      ),
+    );
+  }
+}
