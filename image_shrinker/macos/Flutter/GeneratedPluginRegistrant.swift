8ec6b7a985655033b85a48ac5a4fedd60ca95eed
Kin Yuunan <yuunan.kin@gmail.com>
2023-05-20 09:37:20 +0900

    add video_compress

--- image_shrinker/macos/Flutter/GeneratedPluginRegistrant.swift
+++ image_shrinker/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import desktop_drop
 import file_selector_macos
 import shared_preferences_foundation
+import video_compress
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
 }
