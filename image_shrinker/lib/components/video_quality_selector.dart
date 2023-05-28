4b572f61fd17fe109ad9c2ca3065c4155c52bd18
Kin Yuunan <yuunan.kin@gmail.com>
2023-05-29 08:48:54 +0900

    video quality day2

--- image_shrinker/lib/components/video_quality_selector.dart
+++ image_shrinker/lib/components/video_quality_selector.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_shrinker/models/video_compress_quality.dart';
+import 'package:image_shrinker/providers/config_provider.dart';
 
 class VideoQualitySelector extends ConsumerWidget {
   const VideoQualitySelector({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final current =
+        ref.watch(configProvider.select((value) => value.videoCompressQuality));
+    return DropdownButton(
+        value: current,
+        items: VideoCompressQuality.values
+            .map((e) => DropdownMenuItem(
+                  value: e,
+                  child: Text(e.displayText()),
+                ))
+            .toList(),
+        onChanged: (value) {
+          if (value == null) {
+            return;
+          }
+          final config = ref.read(configProvider);
+          final configNotifier = ref.read(configProvider.notifier);
+          configNotifier.state = config.copyWith(videoCompressQuality: value);
+        });
   }
 }
--- image_shrinker/lib/main.dart
+++ image_shrinker/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:image_shrinker/components/loading/loading_screen.dart';
 import 'package:image_shrinker/components/percentage_slider.dart';
 import 'package:image_shrinker/components/pixel_slider.dart';
 import 'package:image_shrinker/components/shrink_button.dart';
+import 'package:image_shrinker/components/video_quality_selector.dart';
 import 'package:image_shrinker/models/file_mode.dart';
 import 'package:image_shrinker/providers/file_mode_provider.dart';
 import 'package:image_shrinker/providers/loading_provider.dart';
@@ -56,6 +57,7 @@ class HomePage extends ConsumerWidget {
           const FolderSelector(),
           if (imageMode) const PercentageSlider(),
           if (imageMode) const PixelSlider(),
+          if (!imageMode) const VideoQualitySelector(),
           const ShrinkButton(),
           const ChangeModeButton()
         ]),
