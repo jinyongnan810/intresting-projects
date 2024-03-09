e424fe505902a85b346681a993e3e321a64e3077
Kin Yuunan <yuunan.kin@gmail.com>
2024-03-09 09:27:00 +0900

    learning notes

--- react-19-playground/README.md
+++ react-19-playground/README.md
@@ -2,3 +2,37 @@
 - Copied from https://github.com/bradtraversy/react-19-playground
 
 # Notes
+## use() hook
+### fetch api and display data
+- create async function to fetch data.
+- in the component that needs fetch data, use `use(async function)` to call the async function.
+- surround `Suspense` to the component to handle the loading state.
+
+### replace useContext() hook
+- create context using `createContext()`.
+- create a provider that can wrap children with context.
+- in the child component, use `use(context)` to get the context value, which can also be `useContext(context)`.
+- display child component wrapped with provider.
+
+## form actions
+- create form with input fields.
+- add action to the form.
+- in action function, we can get the form data in parameter, and get specific input value using `param.get('inputName')`.
+
+## useFormStatus() hook
+- in the form's submit button, use `useFormStatus()` to get the form status, such as `pending`.
+
+## useFormState() hook
+- in form, use hook like `const [state, formAction] = useFormState(someCallback, initialState);`
+  - send formAction form's `action` attribute.
+- `someCallback` is like `(prevState, formData) => {} `.
+
+## useOptimistic() hook
+- display optimistic UI when user submitting form.
+- the optimistic data is cleared when parent component re-rendered.
+
+## useTransition() hook
+- use `useTransition()` like `const [isPending, startTransition] = useTransition();`
+- wrap state update with `startTransition(() => {})` to mark as transition.
+- use `isPending` to display loading state.
+
--- react-19-playground/src/components/useExampleContext/Theme.jsx
+++ react-19-playground/src/components/useExampleContext/Theme.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, use } from 'react';
+import React, { createContext, useState, use } from "react";
 
 // Create a context object
 const ThemeContext = createContext();
@@ -6,11 +6,11 @@ const ThemeContext = createContext();
 // Create a provider component
 const ThemeProvider = ({ children }) => {
   // State to hold the current theme
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
   // Function to toggle theme
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
@@ -23,22 +23,22 @@ const ThemeProvider = ({ children }) => {
 
 const ThemedCard = () => {
   // Access the theme context using the use() hook
-  const { theme, toggleTheme } = use(ThemeContext);
+  const { theme, toggleTheme } = use(ThemeContext); // note: equal to useContext(ThemeContext)
 
   return (
     <div
       className={`max-w-md mx-auto shadow-md rounded-lg p-6 ${
-        theme === 'light' ? 'bg-white' : 'bg-gray-800'
+        theme === "light" ? "bg-white" : "bg-gray-800"
       }`}
     >
       <h1
         className={`text-2xl mb-3 ${
-          theme === 'light' ? 'text-gray-800' : 'text-white'
+          theme === "light" ? "text-gray-800" : "text-white"
         }`}
       >
         Themed Card
       </h1>
-      <p className={theme === 'light' ? 'text-gray-800' : 'text-white'}>
+      <p className={theme === "light" ? "text-gray-800" : "text-white"}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non neque
         libero. Nullam mattis metus a sapien tempor, sit amet mollis est
         facilisis. Phasellus nec turpis nec dui venenatis vestibulum. Sed
@@ -48,9 +48,9 @@ const ThemedCard = () => {
       {/* Toggle button */}
       <button
         onClick={toggleTheme}
-        className='mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600'
+        className="mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
       >
-        {theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
+        {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
       </button>
     </div>
   );
